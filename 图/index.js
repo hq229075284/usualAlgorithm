453168811f4adecc1a640d194d9e16a2ce1248f3
HanQ <229075284@qq.com>
2018-07-19 18:12:56 +0800

    编写图--广度遍历算法

--- 图/index.js
+++ 图/index.js
@@ -1,3 +1,5 @@
+// struct ->
+
 // {
 //   key:'',
 //   isVisited:false,
@@ -8,27 +10,72 @@
 function Graphic() {
   this.start = null
   this.end = null
-  this.addNode = addNode
-  this.deleteNode = deleteNode
+  this.nodes = []
+  // this.addNode = addNode
+  // this.removeNode = removeNode
+  // this.relateNode = relateNode
+  // this.hasRelation = hasRelation
 }
 
+Graphic.prototype = {
+  addNode: addNode,
+  removeNode: removeNode,
+  relateNode: relateNode,
+  hasRelation: hasRelation,
+}
+
+// 广度遍历
+function hasRelation(startKey, relatedKey) {
+  var startNode = this.nodes.filter(one => one.key === startKey)[0]
+  if (startNode) {
+    let query = [startNode]
+    let hasRelation = false
+    while (query.length > 0) {
+      const one = query.pop()
+      if (one.isVisited) continue
+      if (one.key === relatedKey) {
+        hasRelation = true
+        break
+      }
+      one.isVisited = true
+      one.paths.forEach(node => {
+        if (!node.isVisited) {
+          query.push(node)
+        }
+      })
+    }
+    // reset isVisited state
+    this.nodes.forEach(node => node.isVisited = false)
+    return hasRelation
+  }
+  return false
+}
+
+// 广度遍历
 function relateNode(node, relatedKey) {
   if (this.start) {
     let query = [this.start]
+    let isSuccess = false
     while (query.length > 0) {
-      const one = query[0]
+      const one = query.pop()
       if (one.isVisited) continue
       if (one.key === relatedKey) {
         // 找到关联的key
         one.paths.push(node)
         node.paths.push(one)
+        isSuccess = true
         break
       }
       one.isVisited = true
-      if (one.paths.length > 0) {
-        query.push.apply(query, one.paths)
-      }
+      one.paths.forEach(node => {
+        if (!node.isVisited) {
+          query.push(node)
+        }
+      })
     }
+    // reset isVisited state
+    this.nodes.forEach(node => node.isVisited = false)
+    return isSuccess
   }
   return false
 }
@@ -38,6 +85,19 @@ function addNode(key, nodeValue, relatedKey) {
   if (!this.start) {
     this.start = node
   }
+  this.nodes.push(node)
+  if (relatedKey) {
+    this.relateNode(node, relatedKey)
+  }
 }
 
-function deleteNode() { }
+function removeNode(removeKey) {
+  let deleteIndex = this.nodes.findIndex(node => node.key === removeKey)
+  if (deleteIndex > -1) {
+    let removeNode = this.nodes.splice(deleteIndex, 1)
+    removeNode.paths.forEach(anoterNode => {
+      anoterNode.paths = anoterNode.paths.filter(node => node.key !== removeKey)
+    })
+  }
+  return true
+}
