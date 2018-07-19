e0e48e07246d0501a9d82dabad3e0c2ba666a7cf
HanQ <229075284@qq.com>
2018-07-19 18:12:32 +0800

    优化二叉树

--- 二叉树/index.js
+++ 二叉树/index.js
@@ -1,75 +1,119 @@
-var a = 1
+function addNode(node) {
+  if (this.root) {
+    var currentNode = this.root
+    while (true) {
+      if (this.addRule(currentNode.value, node)) {
+        if (currentNode.leftNode) {
+          currentNode = currentNode.leftNode
+        } else {
+          currentNode.leftNode = {
+            leftNode: null,
+            rightNode: null,
+            value: node
+          }
+          break
+        }
+      } else {
+        if (currentNode.rightNode) {
+          currentNode = currentNode.rightNode
+        } else {
+          currentNode.rightNode = {
+            leftNode: null,
+            rightNode: null,
+            value: node
+          }
+          break
+        }
+      }
+    }
+  } else {
+    this.root = {
+      leftNode: null,
+      rightNode: null,
+      value: node
+    }
+  }
+}
+
+function findNode(root, conditionFn) {
+  var result = []
+  function _findNode(node) {
+    if (node !== null) {
+      // 中序
+      _findNode(node.leftNode)
+      if (conditionFn(node.value)) {
+        result.push(node.value)
+      }
+      _findNode(node.rightNode)
+
+      /* // 先序
+      if (conditionFn(node.value)) {
+        result.push(node.value)
+      }
+      _findNode(node.leftNode)
+      _findNode(node.rightNode) */
+
+      /* // 后序
+      _findNode(node.leftNode)
+      _findNode(node.rightNode)
+      if (conditionFn(node.value)) {
+        result.push(node.value)
+      } */
+    }
+  }
+
+  _findNode(root)
+  return result
+}
+
+function getSmallestNode(currentSubtreeRootNode) {
+  let minNode = currentSubtreeRootNode
+  while (true) {
+    if (minNode.leftNode) {
+      minNode = minNode.leftNode
+    } else {
+      break
+    }
+  }
+  return minNode
+}
+
+function removeNode(node, value, equalFn, conditionFn) {
+  if (node) {
+    if (equalFn(node, value)) {
+      if (!node.leftNode && !node.rightNode) {
+        return null
+      }
+      if (node.leftNode) {
+        return node.leftNode
+      }
+      if (node.rightNode) {
+        return node.rightNode
+      }
+      var rightMinNode = getSmallestNode(node.right)
+      node.value = rightMinNode.value
+      node.right = removeNode(node.right, rightMinNode.value, equalFn, conditionFn)
+      return node
+    }
+    if (conditionFn(node.value, value)) {
+      node.leftNode = removeNode(node.leftNode, value, equalFn, conditionFn)
+      return node
+    }
+    node.rightNode = removeNode(node.rightNode, value, equalFn, conditionFn)
+    return node
+  }
+  return null
+}
 
 function Tree(options) {
   this.root = null
-  this.addNode = function (node) {
-    if (this.root) {
-      var currentNode = this.root
-      while (true) {
-        if (this.addRule(currentNode.value, node)) {
-          if (currentNode.leftNode) {
-            currentNode = currentNode.leftNode
-          } else {
-            currentNode.leftNode = {
-              leftNode: null,
-              rightNode: null,
-              value: node
-            }
-            break
-          }
-        } else {
-          if (currentNode.rightNode) {
-            currentNode = currentNode.rightNode
-          } else {
-            currentNode.rightNode = {
-              leftNode: null,
-              rightNode: null,
-              value: node
-            }
-            break
-          }
-        }
-      }
-    } else {
-      this.root = {
-        leftNode: null,
-        rightNode: null,
-        value: node
-      }
-    }
-  }
-  this.findNode = function (conditionFn) {
-    var result = []
-    function _findNode(node) {
-      if (node !== null) {
-        // 中序
-        _findNode(node.leftNode)
-        if (conditionFn(node.value)) {
-          result.push(node.value)
-        }
-        _findNode(node.rightNode)
-
-        /* // 先序
-        if (conditionFn(node.value)) {
-          result.push(node.value)
-        }
-        _findNode(node.leftNode)
-        _findNode(node.rightNode) */
-
-        /* // 后序
-        _findNode(node.leftNode)
-        _findNode(node.rightNode)
-        if (conditionFn(node.value)) {
-          result.push(node.value)
-        } */
-      }
-    }
-
-    _findNode(this.root)
-    return result
-  }
+  this.addNode = addNode
+  this.findNode = (conditionFn) => findNode(this.root, conditionFn)
   this.addRule = function (currentNode, addNode) {
     return currentNode.id > addNode.id
+  }
+  this.compareRule = function (min, node) {
+    return node.id < min
   }
 }
 
